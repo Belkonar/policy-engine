59b510a9c84403fa1837b1242f864c026fac6b85
joseph Dotson <josephdotson@trollcavedev.com>
2023-03-11 21:57:36 -0500

    basics work, I'll deal with the rest after my demo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PolicyEngineRequest, RuleKind, RuleOp } from "../types";
+import * as jp from 'jsonpath';
 
 @Controller()
 export class AppController {
@@ -9,4 +11,38 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post()
+  runEngine(@Body() body: PolicyEngineRequest): string[] {
+    const permissions: string[] = [];
+
+    for (const policy of body.policies || []) {
+      for (const rule of policy.rules || []) {
+        if (isOp(rule)) {
+          const input = {
+            permissions,
+            data: body.data,
+          };
+
+          const obj = jp.value(input, rule.src);
+
+          if (rule.op === 'eq' && obj === rule.value) {
+            permissions.push(policy.permission);
+            break;
+          }
+
+          if (rule.op === 'contains' && obj.indexOf(rule.value) !== -1) {
+            permissions.push(policy.permission);
+            break;
+          }
+        }
+      }
+    }
+
+    return permissions;
+  }
+}
+
+function isOp(rule: RuleKind): rule is RuleOp {
+  return (rule as RuleOp).op !== undefined;
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import * as jp from 'jsonpath';
 
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log(
+      jp.value(
+        {
+          groups: ['a', 'b'],
+        },
+        '$.groups',
+      ),
+    );
+
     return 'Hello World!';
   }
 }
